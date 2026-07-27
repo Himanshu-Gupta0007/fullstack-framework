b9864af47f3195627c4a3819c6429797d13e8f40
DB <guptahimu90@gmail.com>
2026-07-27 13:41:07 +0530

    font-semibold

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-
-const page = () => {
+const Page = () => {
   return (
     <div>
-      <nav className='flex gap-4 flex-row justify-center items-center  text-blue-500 text-lg font-semibold list-none cursor-pointer'>
-        <li>home</li>
-        <li>about</li>
-        <li>contact</li>
-      </nav>
-    </div>
-  )
-}
+      <nav className="flex gap-4 justify-center items-center text-blue-500 text-lg font-semibold">
+        <ul className="flex gap-4 list-none">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
 
-export default page
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Home Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
