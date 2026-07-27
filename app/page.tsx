12e0a820d45bf39d36aaeffced8d9b7880a0ee21
DB <guptahimu90@gmail.com>
2026-07-27 12:57:02 +0530

    home

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      page
+      <nav className='flex gap-4 flex-row justify-center items-center  text-blue-500 text-lg font-semibold list-none cursor-pointer'>
+        <li>home</li>
+        <li>about</li>
+        <li>contact</li>
+      </nav>
     </div>
   )
 }
