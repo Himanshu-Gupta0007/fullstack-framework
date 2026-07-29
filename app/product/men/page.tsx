f2b21cb8398af523aef6d346949159225cbdd9c2
DB <guptahimu90@gmail.com>
2026-07-29 14:42:52 +0530

    first commit

--- app/product/men/page.tsx
+++ app/product/men/page.tsx
@@ -6,9 +6,11 @@ const page = () => {
       <h1 className="text-4xl font-bold">Men's Product Page</h1>
       <p className="mt-4 text-lg text-gray-600">Explore our exclusive collection of men's products. From stylish apparel to essential accessories, find everything you need to elevate your wardrobe.</p>
     </div>
-
-    
   )
 }
+
+
+  
+
 
 export default page
