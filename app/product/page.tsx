12edb648e9fb24ef28980eadf1017248a555ff2a
DB <guptahimu90@gmail.com>
2026-08-06 12:34:06 +0530

    product id

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
 
-
 const page = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-        
-      <h1 className="text-4xl font-bold">Product Page</h1>
-      <p className="mt-4 text-lg text-gray-600">Welcome to the product page. Here you can find all the details about our products.</p>
-    </div>
+    <div className='text-3xl text-center mt-10'>This is the product
+      sdpage</div>
   )
 }
 
