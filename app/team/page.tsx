d9aa68ea363f3887cd95c8df3ce58c0dfdbddd56
DB <guptahimu90@gmail.com>
2026-08-10 13:59:13 +0530

    add gen ai

---
+++ app/team/page.tsx
@@ -0,0 +1,15 @@
+const Page = () => {
+  return (
+    <div className="min-h-screen p-10">
+      <h1 className="text-3xl font-bold">
+        Gen AI
+      </h1>
+
+      <p className="mt-2 text-gray-600">
+        Ask anything to AI
+      </p>
+    </div>
+  );
+};
+
+export default Page;
