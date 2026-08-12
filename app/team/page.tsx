ba3023c28f7faa56dc25ca98b4e9d44fa27e0c21
DB <guptahimu90@gmail.com>
2026-08-12 13:07:13 +0530

    Invoke LLM

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='bg-blue-500 text-white p-4'>invoke LLM </div>
+    <div className='bg-blue-500 text-white p-4'>invoke LLM
+    <h1>Team</h1>
+    
+     </div>
   )
 }
 
